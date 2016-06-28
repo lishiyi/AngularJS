810924ffd942090cd9c392fefdbcdea8c6a359d6
lishiyi <lishiyiiii@gmail.com>
2016-06-27 20:34:59 -0400

    Meme

---
+++ JSparty/Meme/app.js
@@ -0,0 +1,13 @@
+var main = function() {
+  $('#top-text').keyup(function(){
+    $('.top-caption').text($(this).val());
+  });
+  $('#bottom-text').keyup(function(){
+    $('.bottom-caption').text($(this).val());
+  });
+  $('#image-url').keyup(function(){
+    $('img').attr( {'src' : $(this).val() });
+  });
+};
+ 
+$(document).ready(main);
